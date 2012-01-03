69396f70bf5c5ac6454d752def512dea1c3a599a
Patrick Gerken <do3cc@patrick-gerken.de>
2012-01-03 12:06:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.backtowork',
+      version=version,
+      description="A little event handler to notify you that zope is up and running",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Patrick Gerken',
+      author_email='do3cc@do3.cc',
+      url='http://github.com/collective/',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'five.grok',
+          'zope.processlifetime',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ src/collective/backtowork/__init__.py
@@ -0,0 +1,16 @@
+from five import grok
+from zope.processlifetime import IProcessStarting
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+@grok.subscribe(IProcessStarting)
+def backtowork(event)
+    if os.uname()[0].lower() == 'linux':
+        if os.system('notify-send "Zope up and running"'):
+            logger.error("Could not send notification. Is the CLI tool"
+                         "notify-send available?")
+    else:
+        logger.info("Sorry, no notification support for your OS yet. "
+                    "Care to add it?")
