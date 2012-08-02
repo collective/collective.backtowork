fb7ca9aa8ee79c9e8d4eda9693a5b5d470918598
Philip Bauer <bauer@starzel.de>
2012-08-02 12:37:03 +0200

    add support for notification center in mountain-lion

--- src/collective/backtowork/__init__.py
+++ src/collective/backtowork/__init__.py
@@ -11,7 +11,11 @@ def backtowork(event):
             logger.warning("Could not send notification. Is the CLI tool"
                            "notify-send available?")
     elif osname == 'darwin':
-        if os.system('growlnotify -t "Zope is up and running !" -m "You can go back to work ..."'):
+        if os.path.exists('/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier'):
+            if os.system('/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -message "You can go back to work ..."  -title "Zope is up and running !" '):
+                logger.warning("Could not send notification to notification center. "
+                               "Install terminal-notifier from https://github.com/alloy/terminal-notifier/downloads!")
+        elif os.system('growlnotify -t "Zope is up and running !" -m "You can go back to work ..."'):
             logger.warning("Could not send notification. Are growl and"
                            "growlnotify tool available?")
     else:
