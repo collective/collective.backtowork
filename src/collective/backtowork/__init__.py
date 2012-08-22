1c2bb7c2178c27679c08c49bc3e6bd7a58388379
Patrick Gerken <do3cc@patrick-gerken.de>
2012-08-22 17:10:02 +0300

    Update src/collective/backtowork/__init__.py

--- src/collective/backtowork/__init__.py
+++ src/collective/backtowork/__init__.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 def backtowork(event):
     osname = os.uname()[0].lower()
     if osname == 'linux':
-        if os.system('notify-send "Zope is up and running !"'):
+        if os.system('notify-send "Zope is up and running!"'):
             logger.warning("Could not send notification. Is the CLI tool"
                            "notify-send available?")
     elif osname == 'darwin':
@@ -15,7 +15,7 @@ def backtowork(event):
             if os.system('/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -message "You can go back to work ..."  -title "Zope is up and running !" '):
                 logger.warning("Could not send notification to notification center. "
                                "Install terminal-notifier from https://github.com/alloy/terminal-notifier/downloads!")
-        elif os.system('growlnotify -t "Zope is up and running !" -m "You can go back to work ..."'):
+        elif os.system('growlnotify -t "Zope is up and running!" -m "You can go back to work ..."'):
             logger.warning("Could not send notification. Are growl and"
                            "growlnotify tool available?")
     else:
